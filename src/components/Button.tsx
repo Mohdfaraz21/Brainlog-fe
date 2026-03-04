5b6c623fac373d7ad1a0f0c1989c5ded451302ba
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-03-05 03:35:41 +0530

    building button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ const variantClasses = {
       "secondary": "bg-purple-200 text-purple-600",
 }
 
-const defaultStyles = "px-4 py-2 rounded-md font-light flex item-center"
+const defaultStyles = "px-4 py-2 rounded-md font-light flex justify-center items-center"
 
 export function Button({variant, text, startIcon, onClick,loading, fullWidth}: ButtonProps) {
 
