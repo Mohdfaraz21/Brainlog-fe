b0a49b89dc39c256def4e48c89f42dc9e27e11ac
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-03-05 03:36:24 +0530

    building card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { Button } from "./components/Button";
+import { Card } from "./components/Card";
+import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
   return (
     <>
-      <div className="">
-        <Button variant="primary" text="Share" startIcon={<ShareIcon />} />
+      <div className="flex gap-2 justify-end">
+        <Button variant="primary" text="Add Content" startIcon={<PlusIcon />} />
+        <Button variant="secondary" text="Share" startIcon={<ShareIcon />} />
+      </div>
+      <div className="flex flex-wrap gap-4">
+        <Card
+          title="Project Ideas"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          type="youtube"
+        />
+        <Card
+          title="Project Ideas"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          type="youtube"
+        />
+        <Card
+          title="Project Ideas"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          type="youtube"
+        />
       </div>
     </>
   );
