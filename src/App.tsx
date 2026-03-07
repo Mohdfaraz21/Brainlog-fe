9b2a1560ecf87b4eeeb4c170709f263deb0e1e02
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-03-08 03:17:35 +0530

    auth impelment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import Dashboard from "./pages/dashboard";
-
+import { Signin } from "./pages/signin"
+import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/dashboard"
 function App() {
-<Dashboard />
-
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 }
 
-export default App;
+export default App
